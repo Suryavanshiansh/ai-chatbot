69acf4f493a59163fdc9ed743cd5f242aecedd7f
Suryavanshiansh <suryavanshiansh2007@gmail.com>
2026-04-20 04:29:26 +0530

    Cleanup: Removed local server, using Vercel API only

--- server/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleChat } from "../controllers/chat.controller.js";
-
-const router = express.Router();
-
-router.post("/", handleChat);
-
-export default router;
